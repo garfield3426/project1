57655b5588600b0d69bebff3277915b1ca8dd7a8
毛仁奇 <maorenqi@foxmail.com>
2016-09-29 22:36:28 +0800

    算法

--- phpinfo.php
+++ phpinfo.php
@@ -1,11 +1,8 @@
 <?php
 	//all the infomation
 	echo phpinfo();
-	//add function
-	function addDoctor(){
-		//
-	}
-	function editDoctor(){
-		//
-	}
-?>
+	for($i=0;$i<=3;$i++){
+  echo str_repeat(" ",3-$i);
+  echo str_repeat("*",$i*2+1);
+  echo '<br/>';
+}
