d0b63b43594de6539d29f86a72488935107954d0
maorenqi <maorenqi@foxmail.com>
2016-10-11 22:19:14 +0800

    showip

--- function.php
+++ function.php
@@ -27,3 +27,17 @@ function tree($directory)
 	echo "</ul>\n"; 
 	$mydir->close(); 
 } 
+
+//根据域名显示IP
+$dm=array(
+				'www.purui.cn',
+				'www.p0931.com',
+				' ',
+);	
+//showIp($dm);
+function showIp($dm){
+	foreach($dm as $v){
+		echo $v.'  IP IS:   <font color=\'red\'>'.gethostbyname($v).'</font><br /><hr>';
+	}
+}				
+
